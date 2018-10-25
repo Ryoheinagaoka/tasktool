6897411b7a9f62b9941e2405223699f0754fd044
長岡良平 <nagaoka.ryouhei@ryoheinagaoka.local>
2018-10-25 10:07:38 +0900

    タスク削除　追加　レーン追加機能　実装

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -2,16 +2,24 @@
 <template>
   <div id="app">
   <h1>TaskTool</h1>
-  <form @submit.prevent="addItem">
-<input type="text" v-model="newItem">
-<input type="submit" value="追加">
-  </form>
-<div class=tasks>
+
+<div class=tasks >
+
+  <button @click="addlen()">len追加</button>
+
     <draggable :options="{ group: 'tasks' }" element="ul" v-for="list in tasks">
-      <li v-for="item in list">{{ item }}</li>
+      <h2>{{list.name}}</h2>
+       <li v-for="(item, index) in list.task">
+          <span @click="deleteItem(item,index, list.name)" class="command">[x]</span>
+        {{ item }}
+      </li>
+      <button @click="addItem(index,list.name)">追加</button>
     </draggable>
+
   </div>
 </div>
+
+
 </template>
 
 <!-- javascript -->
@@ -25,16 +33,68 @@ export default {
   data() {
     return {
       tasks: [
-        ['task1-1', 'task1-2', 'task1-3', 'task1-4'],
-        ['task2-1', 'task2-2', 'task2-3', 'task2-4'],
-        ['task3-1', 'task3-2', 'task3-3', 'task3-4']
-      ]
+        {
+          name: "タスク",
+        task: [
+          'task1-1',
+          'task1-2',
+          'task1-3',
+        ]},{
+          name: "作業中",
+          task: [
+            'task2-1',
+            'task2-2',
+            'task2-3',
+          ]},{
+            name: "完了",
+            task:[
+              'task3-1',
+              'task3-2',
+              'task3-3',
+            ]},
+          ],
     }
   },
   methods:{
-    addItem:function(){
-    this.tasks[0].push(this.newItem);
-    this.newItem="";
+    addItem:function(index,name,item){
+    alert("name"+name+item);
+    var i;
+    for(i = 0; i < this.tasks.length; i++){
+      if(this.tasks[i].name == name){
+    var newTask = prompt("タスク名を入力してください");
+        this.tasks[i].task.push(newTask);
+        // 更新用
+        this.tasks.push("aaa");
+        this.tasks.pop("aaa");
+        break;
+      }
+    }
+  },
+    deleteItem:function(item, index, name){
+      alert("item " + item);
+      // this.tasks[1]["task"].splice(index,1);
+      var i;
+      for (i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].name == name) {
+          alert("equal!!!");
+          this.tasks[i].task.splice(index, 1);
+          // 更新用
+          this.tasks.push("aaa");
+          this.tasks.pop("aaa");
+          break;
+        }
+      }
+    },
+    addlen:function(){
+      var newlen = prompt("レーン名を入力してください");
+      var key = this.tasks.length;
+      this.tasks[key] = {};
+      this.tasks[key]["name"] = newlen;
+      this.tasks[key]["task"] = ["sample",];
+      alert(this.tasks[2]+this.tasks[3])
+      // 更新用
+      this.tasks.push("aaa");
+      this.tasks.pop("aaa");
     }
   }
 }
@@ -44,7 +104,7 @@ export default {
 <style>
 .tasks {
   display: flex;
-  justify-content: space-around;
+  justify-content: left;
 }
 ul {
   list-style: none;
